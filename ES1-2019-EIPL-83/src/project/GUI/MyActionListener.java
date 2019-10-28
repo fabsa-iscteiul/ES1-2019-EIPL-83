61685b6acce2fdbe24beb4f078113f07031cd554
fabsa-iscteiul <fabsa@iscte-iul.pt>
2019-10-28 13:16:09 +0000

    Still the start

--- ES1-2019-EIPL-83/src/project/GUI/MyActionListener.java
+++ ES1-2019-EIPL-83/src/project/GUI/MyActionListener.java
@@ -6,14 +6,18 @@ import java.awt.event.ActionListener;
 public class MyActionListener implements ActionListener {
 
 	private String type;
+	private Window window;
 
-	public MyActionListener(String type) {
+	public MyActionListener(String type, Window window) {
 		this.type = type;
+		this.window = window;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (type.equals("longMethod")) {
+			window.createLongMethodWindow();
+		} else if (type.equals("")) {
 
 		}
 	}
--- ES1-2019-EIPL-83/src/project/GUI/Window.java
+++ ES1-2019-EIPL-83/src/project/GUI/Window.java
@@ -1,22 +1,70 @@
 package project.GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 public class Window {
 	private JFrame frame;
 
 	public Window() {
-		frame = new JFrame("ES PROJECT");
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addContent();
-		frame.setVisible(true);
 
 	}
 
 	private void addContent() {
-		JButton longMethod = new JButton();
-		longMethod.addActionListener(new MyActionListener("longMethod"));
+		if (frame != null)
+			frame.dispose();
+		frame = new JFrame("ES PROJECT");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		JButton longMethodButton = new JButton("Long Method");
+		longMethodButton.addActionListener(new MyActionListener("longMethod", this));
+		frame.add(longMethodButton, BorderLayout.SOUTH);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public void createLongMethodWindow() {
+		frame.dispose();
+		frame = new JFrame("Long Method");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setSize(new Dimension(600, 400));
+		frame.setLayout(new GridLayout(2, 2));
+		frame.setLocationRelativeTo(null);
+		JLabel label = new JLabel("How many lines to consider?");
+		frame.add(label);
+		JTextField nrLines = new JTextField();
+		frame.add(nrLines);
+		JButton calculateButton = new JButton("Calculate");
+		calculateButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+		frame.add(calculateButton);
+		JButton backButton = new JButton("Back");
+		backButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addContent();
+			}
+		});
+		frame.add(backButton);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) { // SÓ PARA TESTAR DEPOIS REMOVER ISTO
+		@SuppressWarnings("unused")
+		Window w = new Window();
 	}
 }
