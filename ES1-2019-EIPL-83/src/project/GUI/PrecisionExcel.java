6c5052a9f086dcf323545abf23dfd12748514819
alexandre ferreira <acvpf1-iscteiul.pt>
2019-12-03 23:41:45 +0000

    Documentação das classes do projecto

--- ES1-2019-EIPL-83/src/project/GUI/PrecisionExcel.java
+++ ES1-2019-EIPL-83/src/project/GUI/PrecisionExcel.java
@@ -3,14 +3,42 @@ package project.GUI;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * 
+ * 
+ * 
+ * 
+ * This class is responsible for calculating the accuracy of the Excel file and
+ * will receive a ReadExcel object.
+ * 
+ * @author Members TEAM ES1
+ * @version 1.0 Data : Dec 03-2019
+ *
+ */
+
 public class PrecisionExcel {
 
 	ReadExcel read_excel;
 
+	/**
+	 * constructor and initializes the attributes of the PrecisionExcel object
+	 * 
+	 * @param read_excel
+	 */
 	public PrecisionExcel(ReadExcel read_excel) {
 		this.read_excel = read_excel;
 	}
 
+	/**
+	 * This method will be responsible for calculating the accuracy of long_method
+	 * this will have three int instances that will be accumulative so this method
+	 * will go through all method objects and if its value is long_method equals the
+	 * isPlasma value of the object then the pmd_certai variable will be incremented
+	 * and so on for the other variables until read all method objects at the end
+	 * these variables are multiplied by 100 and divided by the size of the excel
+	 * file to calculate their precision at the end the value of the various
+	 * precisions is printed on the console
+	 */
 	public void calculate_accuracy_longMethod() {
 		this.read_excel.readExel();
 		double pmd_accuracy;
@@ -43,10 +71,20 @@ public class PrecisionExcel {
 		}
 	}
 
+	/**
+	 * 
+	 * This method prints the value o f the excel file size on the console.
+	 */
 	public void size_methods() {
 		System.out.println(this.read_excel.getMethods().size());
 	}
 
+	/**
+	 * main method that tests the calculate_accuracy_longMethod () method of the
+	 * Precision Excel class
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		File excel_file = new File("Long-Method.xlsx");
 		ReadExcel r_e = new ReadExcel(excel_file.toPath().toString());
