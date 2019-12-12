c5d44d42723913a1e3cd898dc951ee1883dd9e69
bgnni-iscteiul <bgnni@iscte-iul.pt>
2019-12-12 20:54:13 +0000

    Atualizar mais uma vez AllTests.java

--- ES1-2019-EIPL-83/src/tests/AllTests.java
+++ ES1-2019-EIPL-83/src/tests/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
 @RunWith((JUnitPlatform.class))
-@SelectClasses({  MethodTest.class, RuleTest.class,MethodTest.class,HandlerTest.class,PrecisionTest.class ,ReadExcelTest.class,MyActionListenerTest.class })
+@SelectClasses({ WindowTest.class, MethodTest.class, RuleTest.class,MethodTest.class,HandlerTest.class,PrecisionTest.class ,ReadExcelTest.class,MyActionListenerTest.class })
 public class AllTests {
 
 }
